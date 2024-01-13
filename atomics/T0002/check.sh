4f381638fb584837b2a99d34f66c42a0ecdf4975
user <ramon.pinuaga@gmail.com>
2024-01-13 12:06:21 +0100

    Added sample container

--- atomics/T0002/check.sh
+++ atomics/T0002/check.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+apt update; apt install curl
 curl -L "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" -o /tmp/kubectl
 chmod 755 /tmp/kubectl
+echo "Permission to create pods: "
 /tmp/kubectl auth can-i create pods
