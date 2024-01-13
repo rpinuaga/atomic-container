b52674524989b3f1087daf2e0a042e31914a886d
user <ramon.pinuaga@gmail.com>
2024-01-13 15:54:20 +0100

    Fixed check.sh

--- atomics/T0003/check.sh
+++ atomics/T0003/check.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 apt update; apt install -y libcap2-bin
+echo "Container running as root: "
+id
+echo "CAP_SYS_MODULE capability: "
 capsh --print | grep cap_sys_module
--- atomics/T0004/check.sh
+++ atomics/T0004/check.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-apt update; apt install libcap2-bin util-linux
+apt update; apt install -y libcap2-bin util-linux
+echo "Container running as root: "
+id
+echo "CAP_SYS_ADMIN capability: "
 capsh --print | grep cap_sys_admin
+echo "Block device exposed in the container: "
 lsblk | grep part
