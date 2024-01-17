fe180211107b7e02d03a4dc08f5a7d332e72829e
Ramon Pinuaga <129771135+rpinuaga-sky@users.noreply.github.com>
2024-01-17 17:40:39 +0100

    Update check.sh

--- atomics/T0001/check.sh
+++ atomics/T0001/check.sh
@@ -4,7 +4,7 @@ echo "Container running as root: "
 id
 echo "CAP_SYS_ADMIN capability: "
 capsh --print | grep cap_sys_admin
-echo "Cgroups v1 enabled: "
+echo "Cgroups v1 enabled (+rw): "
 mount | grep "/sys/fs/cgroup/memory type cgroup (rw"
 echo "A path to the OverlayFS mounted in container: "
 mount | sed -n 's/.*\perdir=\([^,]*\).*/\1/p'
