c5a7516a483a8be0555ae9dd2242a08b237555e8
Ramon Pinuaga <ramon.pinuaga@gmail.com>
2024-04-03 18:20:03 +0200

    Create check.sh

---
+++ atomics/T0015/check.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo "Instaling dependencies: "
+apt update -qq; apt install -qq -y libcap2-bin 
+echo "Container running as root: "
+id
+echo "CAP_SYS_ADMIN capability: "
+capsh --print | grep cap_sys_admin
+echo "/proc/sys/fs/binfmt_misc/ exposed"
+ls -al /proc/sys/fs/binfmt_misc/
